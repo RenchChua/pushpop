9fbea6f2f11a18bf64c807b10ec447bebfae1f51
Rencheng Chua <rench00@gmail.com>
2016-08-11 21:56:32 +0800

    initial commit

--- spec/ScriptSpec.js
+++ spec/ScriptSpec.js
@@ -1,104 +1,49 @@
-describe('panda',function(){
-  it('is happy',function(){
-    expect(panda).toBe('happy');
-  });
-});
+describe("function to keep repeats in array", function () {
+  var not_arr = "abc";
+  var no_repeat_simple = [1,2,3];
+  var no_repeat_not_simple = [2, 1, 3];
+  var simple_one_repeat_arr = [1, 1, 2, 3];
+  var not_simple_one_repeat_arr = [1, 2, 1, 3];
+  var not_simple_two_repeat_arr = [1, 2, 1, 2, 3];
+  var not_simple_many_repeat_arr = [1,2,1,2,1,2,3,3,4,3,1,1,3];
+  var not_simple_many_repeat_order_wrong_arr = [2,1,2,1,2,3,3,4,3,1,1,3];
 
-describe('Sun rises',function(){
-  it('always comes from the east',function(){
-    expect(sunrise_from_east).toBe(true);
-    expect(sunrise_from_west).toBe(false);
-  });
-});
+  // it("didn't get array", function () {
+  //   expect(repeatArr(not_arr)).toThrowError( "Not Array!" );
+  // });
 
-describe('X and Y',function(){
-  it('is the same',function(){
-    expect(x).toBe(y);
-  });
-});
-
-describe('foo and bar',function(){
-  it('is not an equal object',function(){
-    expect(foo).not.toEqual(bar);
-  });
-});
-
-describe('The phrase',function(){
-  it('has word "cali" in it',function(){
-    expect(phrase).toMatch(/cali/);
-  });
-});
-
-describe('The calculation',function(){
-  it('will return infinity number',function(){
-    expect(calculation).toBeGreaterThan(Infinity);
-  });
-});
-
-describe("The do this's inner function", function() {
-  it("should not call the callBack if the first arg is false", function() {
-    doThis(false, function() {
-      fail("Callback has been called");
-    });
-  });
-});
-
-describe("Person", function() {
-  it("needs at least have at least name, age and gender", function() {
-    var new_person = new Person();
-    var person_keys = Object.keys(new_person).sort();
-
-    expect(person_keys).toEqual(jasmine.arrayContaining(['name', 'age', 'gender']));
-  });
-});
-
-describe('getName',function(){
-  var new_person = new Person('test');
-
-  it('is called and returned its name',function(){
-    spyOn(new_person, 'getName');
-    new_person.getName();
-    expect(new_person.getName).toHaveBeenCalled();
-    expect(new_person.name).toBe('test');
-  });
-});
-
-describe('setAge',function(){
-  var foo;
-
-  beforeEach(function() {
-    foo = new Person('new', 25);
-    spyOn(foo, 'setAge').and.callThrough();
-    foo.setAge(2);
+  it("didn't get array", function () {
+    expect(function(){repeatArr(not_arr);}).toThrowError( "Not Array!" );
   });
 
-  it('is called',function(){
-    expect(foo.setAge).toHaveBeenCalled();
+  it("got simple array with no repeats", function () {
+    expect(repeatArr(no_repeat_simple).length).toBe(0);
   });
 
-  it('changes foo ages', function() {
-    expect(foo.age).toBe(27);
-  })
-
-  it('was expecting a number in its argument', function() {
-    expect(foo.setAge).toHaveBeenCalledWith(jasmine.any(Number));
-  })
-});
-
-describe('getAge',function(){
-  var foo, result;
-
-  beforeEach(function() {
-    foo = new Person('new', 25);
-    spyOn(foo, 'getAge').and.callThrough();
-    result = foo.getAge();
+  it("got not simple array with no repeats", function () {
+    expect(repeatArr(no_repeat_not_simple).length).toBe(0);
   });
 
-  it('is called',function(){
-    expect(foo.getAge).toHaveBeenCalled();
+  it("got simple array with one repeat", function () {
+    expect(repeatArr(simple_one_repeat_arr)).toEqual([1]);
   });
 
-  it('returns a number', function() {
-    expect(result).toEqual(jasmine.any(Number));
-  })
+  it("got not simple array with one repeat", function () {
+    expect(repeatArr(not_simple_one_repeat_arr)).toEqual([1]);
+  });
+
+  it("got not simple array with two repeats", function () {
+    expect(repeatArr(not_simple_two_repeat_arr)).toEqual([1, 2]);
+  });
+
+  it("got not simple array with many repeats", function () {
+    expect(repeatArr(not_simple_many_repeat_arr)).toEqual([1, 2, 3]);
+  });
+
+  it("got not simple array with many repeats order wrong", function () {
+    expect(repeatArr(not_simple_many_repeat_order_wrong_arr)).toEqual([1, 2, 3]);
+  });
+
+
+
 });
--- src/Script.js
+++ src/Script.js
@@ -1,59 +1,19 @@
-var panda = 'happy';
 
-// test if sunrise comes from east
-var sunrise_from_east = true;
-var sunrise_from_west = false;
-
-// test if x and y is the same
-var x = 999;
-var y = 999;
-
-// test if object foo and bar is not an 'equal' object
-var foo = {
-  a: 12,
-  b: 34
-};
-var bar = {
-  a: 13,
-  b: 34
-};
-
-// test if there's a word cali in the phrase given
-var phrase = "supercalifragilisticexpialidocious"
-
-// test if calculation is greater than any number
-var calculation = 1000 / 0;
-
-// check if an inner function won't be called if checkTrue is false
-var doThis = function(checkTrue, thenThis) {
-  if (checkTrue) {
-    thenThis();
-  }
-};
-
-// test if a Person object have at least name, age and gender
-function Person(name, age, gender) {
-  this.name = name;
-  this.age = age;
-  this.gender = gender;
-  this.getName = function() {
-    return this.name;
-  };
-  this.setAge = function(num) {
-    this.age += num;
-  }
-  this.getAge = function() {
-    return this.age
+function repeatArr(arr) {
+  if(Array.isArray(arr) === false ){
+    throw new Error("Not Array!");
+  }else{
+    // var sorted_arr = arr.slice().sort();
+    var count = {};
+    results_arr = [];
+    for (var i = 0; i < arr.length; i++) {
+      var item = arr[i];
+      count[item] = count[item] >=1 ? count[item] += 1 : 1;
+      if(count[item] == 2){
+        results_arr.push(item);
+      }
+    }
+    results_arr = results_arr.sort();
+    return results_arr;
   }
 }
-
-// test if Person.getName is called, it will return person's name
-// test both within one test spec
-
-// test if Person.addAge is called, it will changes Person's age
-// also test if the function can be called
-// pass along newly created object across multiple specs
-
-// add spec from previous suite to check if age accepts a number
-
-// check if getAge method can be called and returns a number output
