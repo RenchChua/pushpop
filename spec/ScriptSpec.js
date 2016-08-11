1de9cf6d8e0e8113bf64dea54379e9ab13ad1908
Rencheng Chua <rench00@gmail.com>
2016-08-12 00:24:26 +0800

    check all elements are integers and also added comments

--- spec/ScriptSpec.js
+++ spec/ScriptSpec.js
@@ -7,41 +7,51 @@ describe("function to keep repeats in array", function () {
   var not_simple_two_repeat_arr = [1, 2, 1, 2, 3];
   var not_simple_many_repeat_arr = [1,2,1,2,1,2,3,3,4,3,1,1,3];
   var not_simple_many_repeat_order_wrong_arr = [4,4,2,3,2,3,5,3,4,3,1,1,3];
+  var got_non_integer = [1, 2,2, 5, 4, 5, 3, 1, 'one', 6, 3, 1, 'blah'];
+  var empty_arr = [];
 
   // it("didn't get array", function () {
-  //   expect(repeatArr(not_arr)).toThrowError( "Not Array!" );
+  //   expect(getDuplicate(not_arr)).toThrowError( "Not Array!" );
   // });
 
   it("didn't get array", function () {
-    expect(function(){repeatArr(not_arr);}).toThrowError( "Not Array!" );
+    expect(function(){getDuplicate(not_arr);}).toThrowError( "Not Array!" );
   });
 
   it("got simple array with no repeats", function () {
-    expect(repeatArr(no_repeat_simple).length).toBe(0);
+    expect(getDuplicate(no_repeat_simple).length).toBe(0);
   });
 
   it("got not simple array with no repeats", function () {
-    expect(repeatArr(no_repeat_not_simple).length).toBe(0);
+    expect(getDuplicate(no_repeat_not_simple).length).toBe(0);
   });
 
   it("got simple array with one repeat", function () {
-    expect(repeatArr(simple_one_repeat_arr)).toEqual([1]);
+    expect(getDuplicate(simple_one_repeat_arr)).toEqual([1]);
   });
 
   it("got not simple array with one repeat", function () {
-    expect(repeatArr(not_simple_one_repeat_arr)).toEqual([1]);
+    expect(getDuplicate(not_simple_one_repeat_arr)).toEqual([1]);
   });
 
   it("got not simple array with two repeats", function () {
-    expect(repeatArr(not_simple_two_repeat_arr)).toEqual([1, 2]);
+    expect(getDuplicate(not_simple_two_repeat_arr)).toEqual([1, 2]);
   });
 
   it("got not simple array with many repeats", function () {
-    expect(repeatArr(not_simple_many_repeat_arr)).toEqual([1, 2, 3]);
+    expect(getDuplicate(not_simple_many_repeat_arr)).toEqual([1, 2, 3]);
   });
 
   it("got not simple array with many repeats order wrong", function () {
-    expect(repeatArr(not_simple_many_repeat_order_wrong_arr)).toEqual([1, 2, 3, 4]);
+    expect(getDuplicate(empty_arr)).toEqual([]);
+  });
+
+  it("has non-integer", function () {
+    expect(function(){getDuplicate(got_non_integer);}).toThrowError( "Not all integers" );
+  });
+
+  it("got empty array", function () {
+    expect(getDuplicate(not_simple_many_repeat_order_wrong_arr)).toEqual([1, 2, 3, 4]);
   });
 
 
--- src/Script.js
+++ src/Script.js
@@ -1,5 +1,5 @@
 
-function repeatArr(arr) {
+function getDuplicate(arr) {
   if(Array.isArray(arr) === false ){
     throw new Error("Not Array!");
   }else{
@@ -8,12 +8,21 @@ function repeatArr(arr) {
     results_arr = [];
     for (var i = 0; i < arr.length; i++) {
       var item = arr[i];
+      if(!Number.isInteger(item)){
+        throw new Error("Not all integers");
+      }
+      //makes every item in the array a property of count object. if value of that property is >= 1, then add one to that value of that property, else set it to be 1
       count[item] = count[item] >=1 ? count[item] += 1 : 1;
+
+      // if the value of the property is 2, then push it to the results_arr, if it's more than 2, ignore, cos already pushed. If less than 2 also ignore, cos no duplicate. 
       if(count[item] == 2){
         results_arr.push(item);
       }
     }
     results_arr = results_arr.sort();
+    console.log(count);
     return results_arr;
   }
 }
+
+getDuplicate([2,4,2,3,5,3,5,6,3,2,1,3,1])
