234479dc0d9db246008d5aab6b1774c38699ebc5
Rencheng Chua <rench00@gmail.com>
2016-08-12 10:31:44 +0800

    make it dry

--- src/Script.js
+++ src/Script.js
@@ -1,18 +1,8 @@
 function pushPop(initialArr, pushArr, num_to_pop) {
+  if (!Array.isArray(initialArr)) {
+    initialArr = []
+  }
   if (Array.isArray(initialArr) && Array.isArray(pushArr)) {
-    if (pushArr.length !== 0) {
-      for (i = 0; i <= pushArr.length; i++) {
-        initialArr.push(pushArr.shift());
-      }
-    }
-    if (Number.isInteger(num_to_pop) && num_to_pop > 0) {
-      for (i = 1; i <= num_to_pop; i++) {
-        console.log(initialArr);
-        initialArr.pop();
-      }
-    }
-  }else if(!Array.isArray(initialArr)){
-    initialArr = [];
     if (pushArr.length !== 0) {
       for (i = 0; i <= pushArr.length; i++) {
         initialArr.push(pushArr.shift());
